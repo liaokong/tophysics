55e4711fba60e468c55db18eb4be146f8c44afb0
liaojun <286121837@qq.com>
2017-07-28 16:26:27 +0800

    修改样式

--- program/src/components/tpls/Action_top.vue
+++ program/src/components/tpls/Action_top.vue
@@ -1,5 +1,10 @@
 <template>
 <div class="action-tops" id="nav-top">
+<ul>
+  <li>A</li>
+  <li>B</li>
+  <li>L</li>
+</ul>
 </div>
 </template>
 
@@ -15,17 +20,33 @@ export default {
   methods: {
   },
   created () {
-    this.getLinks()
+    // this.getLinks()
   }
 }
 </script>
-<style>
+<style lang="scss">
 .action-tops{
   position: fixed;
   top: 0;
   height: 5%;
   width: 100%;
   display: none;
-  background-color: yellow;
+  background-color: rgba(100,100,100,0.3);
+  ul{
+    margin-left: 0;
+    margin-top: 1.3%;
+    width: 100%;
+    font-size: 1.3rem;
+    li:first-child{
+      border-left: none;
+    }
+    li{
+      width: 33.3%;
+      height: 100%;
+      list-style: none;
+      float: left;
+      border-left: solid thin #ffffff;
+    }
+  }
 }
 </style>
